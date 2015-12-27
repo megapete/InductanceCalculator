3eb8b7dd51fe2df13664bde9e5857ae8821aaa78
PeterCoolAssHuber <peterhub@peteserver.local>
2015-12-27 07:35:50 -0500

    Initial Commit

---
+++ InductanceCalculator/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  InductanceCalculator
+//
+//  Created by PeterCoolAssHuber on 2015-12-27.
+//  Copyright © 2015 Peter Huber. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+

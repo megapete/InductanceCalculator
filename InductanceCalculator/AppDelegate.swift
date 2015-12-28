50300db32679e9b721442fbc505f9110cd729b87
Peter Huber <peterhubercottage@peters-imac.local>
2015-12-28 16:01:52 -0500

    Adding data for job 016

--- InductanceCalculator/AppDelegate.swift
+++ InductanceCalculator/AppDelegate.swift
@@ -68,13 +68,52 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         let ground = PCH_SectionData(sectionID: "0")
         
         // Job016 HV data
-        let capFirstDisk = 1.4923E-9
-        let capOtherDisks = 1.6185E-9
-        let capToShield = 6.5814E-12
-        let capToTank = 1.0259E-22
-        let resPerDisk = 0.19727
-        let numDisks = 60
-         
+        let hv_capFirstDisk = 1.4923E-9
+        let hv_capOtherDisks = 1.6185E-9
+        let hv_capToShield = 6.5814E-12
+        let hv_capToTank = 1.0259E-22
+        let hv_resPerDisk = 0.19727
+        let hv_numDisks = 60.0
+        let hv_turnsPerDisk = 3200.0 / hv_numDisks
+        // start at the top disk (the one that will be shot)
+        var hv_diskRect = NSMakeRect(25.411 / 2.0 * 25.4/1000.0, (2.75 + 32.495 - 0.3488) * 25.4/1000.0, 5.148 * 25.4/1000.0, 0.3488 * 25.4/1000.00)
+        let hv_diskPitch = (32.495 - 0.3488) * 25.4/1000.0 / 59.0
+        let hv_J = 2.4056 * hv_turnsPerDisk / Double(hv_diskRect.size.width * hv_diskRect.size.height)
+        let hv_ID = "HV"
+        
+        // Job016 core data
+        let windowHt = 1.1
+        let coreRadius = 0.141
+        
+        var hvCoil = [PCH_DiskSection]()
+        
+        for _ in 1...Int(hv_numDisks)
+        {
+            hvCoil.append(PCH_DiskSection(diskRect: hv_diskRect, N: hv_turnsPerDisk, J: hv_J, windHt: windowHt, coreRadius: coreRadius))
+            hv_diskRect = hv_diskRect.offsetBy(dx: 0.0, dy:CGFloat(-hv_diskPitch))
+        }
+        
+        // Job016 LV data
+        let lv_capFirstDisk = 1.4734E-10
+        let lv_capOtherDisks = 1.8072E-10
+        let lv_capToShield = 3.9534E-11
+        let lv_capToCore = 3.2097E-11
+        let lv_resPerDisk = 1.3909E-3
+        let lv_numDisks = 16.0
+        let lv_turnsPerDisk = 1.0
+        // start at the top disk (the one that will be shot)
+        var lv_diskRect = NSMakeRect(14.1 / 2.0 * 25.4/1000.0, (3.921 + 32.065) * 25.4/1000.0, 0.296 * 25.4/1000.0, 1.913 * 25.4/1000.00)
+        let lv_diskPitch = (32.065 * 25.4/1000.0 / 16)
+        let lv_J = 481.13 * lv_turnsPerDisk / Double(hv_diskRect.size.width * hv_diskRect.size.height)
+        let lv_ID = "LV"
+        
+        var lvCoil = [PCH_DiskSection]()
+        
+        for _ in 1...Int(lv_numDisks)
+        {
+            lvCoil.append(PCH_DiskSection(diskRect: lv_diskRect, N: lv_turnsPerDisk, J: lv_J, windHt: windowHt, coreRadius: coreRadius))
+            lv_diskRect = lv_diskRect.offsetBy(dx: 0.0, dy:CGFloat(-lv_diskPitch))
+        }
         
     }
 
